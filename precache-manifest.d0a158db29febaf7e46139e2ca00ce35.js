9a334f4a889429723646226b3a9da46486aaa31d
AndreiKravets <tetamoed1@gmail.com>
2019-03-07 11:28:30 +0200

    Updates

---
+++ precache-manifest.d0a158db29febaf7e46139e2ca00ce35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "80b777ea0b2a4ac3e2cb",
+    "url": "/fenix/static/js/runtime~main.80b777ea.js"
+  },
+  {
+    "revision": "a61ee85663a4083c8f75",
+    "url": "/fenix/static/js/main.a61ee856.chunk.js"
+  },
+  {
+    "revision": "2588b9a3f0d3a168fc29",
+    "url": "/fenix/static/js/1.2588b9a3.chunk.js"
+  },
+  {
+    "revision": "a61ee85663a4083c8f75",
+    "url": "/fenix/static/css/main.dd20baff.chunk.css"
+  },
+  {
+    "revision": "18ae3c129e7bbe4a110310a6aeca1d9a",
+    "url": "/fenix/index.html"
+  }
+];
