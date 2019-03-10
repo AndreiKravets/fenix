61fc862da0addb2a70a0ef6f85c1f1e7451389e8
AndreiKravets <tetamoed1@gmail.com>
2019-03-10 11:07:17 +0200

    Updates

--- precache-manifest.465f2df5ecf6c40c544fb78a3c00b6e2.js
+++ precache-manifest.465f2df5ecf6c40c544fb78a3c00b6e2.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/fenix/static/js/runtime~main.80b777ea.js"
   },
   {
-    "revision": "f87c26f72a2c69033868",
-    "url": "/fenix/static/js/main.f87c26f7.chunk.js"
+    "revision": "04ecab2c6c81e4e93dc0",
+    "url": "/fenix/static/js/main.04ecab2c.chunk.js"
   },
   {
     "revision": "2588b9a3f0d3a168fc29",
     "url": "/fenix/static/js/1.2588b9a3.chunk.js"
   },
   {
-    "revision": "f87c26f72a2c69033868",
+    "revision": "04ecab2c6c81e4e93dc0",
     "url": "/fenix/static/css/main.a001706a.chunk.css"
   },
   {
-    "revision": "8e7d410609698400c89b36f3079810f3",
+    "revision": "88d2f62b01679cf5241df8a8fc8d7720",
     "url": "/fenix/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/fenix/precache-manifest.851af5827510a1635cc5bbcf3e7f20f7.js"
+  "/fenix/precache-manifest.465f2df5ecf6c40c544fb78a3c00b6e2.js"
 );
 
 workbox.clientsClaim();
