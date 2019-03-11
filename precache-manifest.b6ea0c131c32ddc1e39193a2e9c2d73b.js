04cfc69940f7e33fd8bf1896d42b3e3e961005ed
AndreiKravets <tetamoed1@gmail.com>
2019-03-11 17:57:08 +0200

    Updates

--- precache-manifest.b6ea0c131c32ddc1e39193a2e9c2d73b.js
+++ precache-manifest.b6ea0c131c32ddc1e39193a2e9c2d73b.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/fenix/static/js/runtime~main.80b777ea.js"
   },
   {
-    "revision": "a616607313a0220989b0",
-    "url": "/fenix/static/js/main.a6166073.chunk.js"
+    "revision": "d622629102c679d8f5f6",
+    "url": "/fenix/static/js/main.d6226291.chunk.js"
   },
   {
     "revision": "2588b9a3f0d3a168fc29",
     "url": "/fenix/static/js/1.2588b9a3.chunk.js"
   },
   {
-    "revision": "a616607313a0220989b0",
-    "url": "/fenix/static/css/main.d0b8fa6f.chunk.css"
+    "revision": "d622629102c679d8f5f6",
+    "url": "/fenix/static/css/main.90fed15e.chunk.css"
   },
   {
-    "revision": "e93965e7db0713db7e1754d9e83d2914",
+    "revision": "74ea8e4a24c94b4ce37385b8c8dbfd72",
     "url": "/fenix/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/fenix/precache-manifest.9f0281d58d4cf671edb4bea518fa1ee6.js"
+  "/fenix/precache-manifest.b6ea0c131c32ddc1e39193a2e9c2d73b.js"
 );
 
 workbox.clientsClaim();
