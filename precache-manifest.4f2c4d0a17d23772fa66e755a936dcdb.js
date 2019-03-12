4bf9bb203f894f3f5a9d623227b272a8ef6e2c7d
AndreiKravets <tetamoed1@gmail.com>
2019-03-12 13:54:26 +0200

    Updates

--- precache-manifest.4f2c4d0a17d23772fa66e755a936dcdb.js
+++ precache-manifest.4f2c4d0a17d23772fa66e755a936dcdb.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/fenix/static/js/runtime~main.80b777ea.js"
   },
   {
-    "revision": "2d864523e08e2f730ae7",
-    "url": "/fenix/static/js/main.2d864523.chunk.js"
+    "revision": "b2c0da6eccfa54fea215",
+    "url": "/fenix/static/js/main.b2c0da6e.chunk.js"
   },
   {
     "revision": "2588b9a3f0d3a168fc29",
     "url": "/fenix/static/js/1.2588b9a3.chunk.js"
   },
   {
-    "revision": "2d864523e08e2f730ae7",
-    "url": "/fenix/static/css/main.2b67e71d.chunk.css"
+    "revision": "b2c0da6eccfa54fea215",
+    "url": "/fenix/static/css/main.1c720460.chunk.css"
   },
   {
-    "revision": "af98280095aa7e5d18cd8ae68cf1e440",
+    "revision": "a624f4f7670257a903c1c533361c7382",
     "url": "/fenix/index.html"
   }
 ];
